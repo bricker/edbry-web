f0120756bc5d5e3a57d59599224e56b4fde2a218
Bryan Ricker <bricker88@gmail.com>
2011-12-07 20:04:15 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdbryWeb::Application.config.secret_token = '6ef63c4b4585d1f960db5515a0632d035ff1b482a9d21196bda908b4aa0e8c86bab78c5345bbba99f26a62c836196ef4f2f8a06036801872e6b417f99b8ff399'
