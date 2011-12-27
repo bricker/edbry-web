b240dda880505431305883ca738f8fa3265a7cd5
Bryan Ricker <bricker88@gmail.com>
2011-12-27 15:48:11 -0800

    WIP: Attachment/Project features

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,2 +1,10 @@
 class Attachment < ActiveRecord::Base
+  belongs_to :attachable, polymorphic: true
+  
+  # Paperclip
+  has_attached_file :asset,
+                    :styles => { :medium => "140x140#", :small => "125x125#", :tiny => "60x60#" },
+                    :default_url => "http://thebirn.com/images/defaults/dj_pic_default_:style.jpg",
+                    :url => "http://thebirn.com/avatars/user/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/avatars/user/:id/:style/:basename.:extension"
 end
